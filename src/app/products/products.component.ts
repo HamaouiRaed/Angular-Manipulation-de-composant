31b832dc3ed125a2aa2ff8ea2405e3bcf1baa47d
HamaouiRaed <hamaouiraed@gmail.com>
2021-10-10 01:34:36 +0200

    button like and buy

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,19 +7,27 @@ import { Product } from '../models/Product';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  listProdcut!: Product[];
-    
-  constructor() { 
-   
+  listProdcut!:Product[];
+
+  constructor() {
+
   }
 
   ngOnInit(): void {
-    this.listProdcut=[
-      {id: 1, title: "T-shirt 1", price: 18, quantity: 0, like: 0},
-      {id: 2, title: "T-shirt 2", price: 21, quantity: 10, like: 0},
-      {id: 3, title: "T-shirt 3", price: 16, quantity: 8, like: 0},
-      ]
-      console.log(this.listProdcut);
+    this.listProdcut=[ { id: 1, title: "T-shirt 1", price: 18, quantity: 0, like: 0 },
+      { id: 2, title: "T-shirt 2", price: 21, quantity: 10, like: 0 },
+      { id: 3, title: "T-shirt 3", price: 16, quantity: 8, like: 0 },
+    ];
+    console.log(this.listProdcut);
+    
   }
 
+  buy(i: number) {
+    this.listProdcut[i].quantity = this.listProdcut[i].quantity- 1;
+    console.log(this.listProdcut[i].quantity);
+  }
+  like (i:number){
+    this.listProdcut[i].like=this.listProdcut[i].like++;
+    console.log(this.listProdcut[i].like);
+  }
 }
